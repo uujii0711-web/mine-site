3ddbae0da079267d8493e78cc20a1077512a4f3e
Usukhbayar Batmunkh <uujii0711@gmail.com>
2026-01-29 13:13:23 +0800

    Update site layout and popup

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 function openLocation() {
-  fetch('data/location1.json')
+  fetch("data/location1.json")
     .then(res => res.json())
-    .then(data => {
-      const panel = document.getElementById('panel');
-      panel.innerHTML = `<h3>${data.name}</h3>`;
-
-      data.inspections.forEach(i => {
-        panel.innerHTML += `
-          <div class="date">
-            <b>${i.date}</b><br>
-            ${i.note}<br>
-            ${i.photos.map(p => `<img src="img/${p}" width="100%">`).join('')}
-          </div>
-        `;
-      });
-
-      panel.classList.remove('hidden');
-    })
-    .catch(err => {
-      console.error("Data load error:", err);
-      alert("Өдрийн мэдээлэл ачаалагдсангүй!");
-    });
+    .then(data => openPopup(data))
+    .catch(() => alert("Өдрийн мэдээлэл ачаалагдсангүй"));
 }
 
+function openPopup(data) {
+  const body = document.getElementById("popup-body");
+
+  body.innerHTML = `
+    <p><b>Огноо:</b> ${data.date}</p>
+    <p><b>Байршил:</b> ${data.location}</p>
+    ${data.photos.map(p => `<img src="img/${p}">`).join("")}
+  `;
+
+  document.getElementById("popup").classList.remove("hidden");
+}
+
+function closePopup() {
+  document.getElementById("popup").classList.add("hidden");
+}
