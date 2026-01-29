ac4602bd86a3b29333d945ba800307c727493b82
Usukhbayar Batmunkh <uujii0711@gmail.com>
2026-01-29 13:29:53 +0800

    Update site layout and popup

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 function openLocation() {
   fetch("data/location1.json")
-    .then(res => res.json())
-    .then(data => openPopup(data))
-    .catch(() => alert("Өдрийн мэдээлэл ачаалагдсангүй"));
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("JSON уншигдсангүй");
+      }
+      return response.json();
+    })
+    .then(data => showPopup(data))
+    .catch(err => {
+      alert("Өдрийн мэдээлэл ачаалагдсангүй");
+      console.error(err);
+    });
 }
 
-function openPopup(data) {
+function showPopup(data) {
   const body = document.getElementById("popup-body");
 
   body.innerHTML = `
     <p><b>Огноо:</b> ${data.date}</p>
     <p><b>Байршил:</b> ${data.location}</p>
-    ${data.photos.map(p => `<img src="img/${p}">`).join("")}
+    ${data.photos.map(p => `<img src="${p}">`).join("")}
   `;
 
   document.getElementById("popup").classList.remove("hidden");
